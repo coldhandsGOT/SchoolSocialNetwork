e3e8f8fca0d892f45ecad7b90f13a046cdb3d90c
coldhandsGOT <midearthbattle@gmail.com>
2018-05-01 20:32:31 +0200

    v7.1

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -7,27 +7,14 @@
 
         @include('profile.widgets.header')
 
-
-
-
-        @if ($can_see)
             <div class="container profile-main">
-                <div class="row">
-                    <div class="col-xs-12">
-                        @if(Session::has('alert-success'))
-                            <div class="alert alert-success"><i class="fa fa-check" aria-hidden="true"></i> <strong>{!! session('alert-success') !!}</strong></div>
-                        @endif
-                        @if(Session::has('alert-danger'))
-                            <div class="alert alert-danger"><i class="fa fa-times" aria-hidden="true"></i> <strong>{!! session('alert-danger') !!}</strong></div>
-                        @endif
-                    </div>
-                </div>
+         
                 <div class="row">
                     <div class="col-md-3">
                         @include('profile.widgets.information')
                     </div>
                     <div class="col-xs-12 col-md-3 pull-right">
-                        @include('profile.widgets.user_follow_counts')
+                       
                         <div class="hidden-sm hidden-xs">
                            
                         </div>
@@ -37,14 +24,6 @@
                     </div>
                 </div>
             </div>
-        @else
-            <div class="container">
-                <div class="alert-message alert-message-default">
-                    <h4>{{ '@'.$user->username."'s" }} profile is private.</h4>
-                    <p>Please follow to see {{ '@'.$user->username."'s" }} profile.</p>
-                </div>
-            </div>
-        @endif
     </div>
 
 
