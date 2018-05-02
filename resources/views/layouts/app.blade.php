9c1b9855f645b83b241da2d20a4e12ab64a716d1
coldhandsGOT <midearthbattle@gmail.com>
2018-05-02 17:56:55 +0200

    Alpha-V0.1..2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     <img src="{{ asset('images/logo.png') }}" alt="" />
                 </a>
             </div>
