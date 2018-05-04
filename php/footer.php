10824598b75506c7c5d743c94c26637b0191460f
coldhandsGOT <midearthbattle@gmail.com>
2018-05-04 23:34:48 +0200

    update css

---
+++ php/footer.php
@@ -0,0 +1,23 @@
+
+
+<script src='../plugins/jquery/jquery-2.1.4.min.js'></script>
+<script src='../plugins/pace-master/pace.min.js'></script>
+<script src='../plugins/bootstrap/js/bootstrap.min.js'></script>
+<script src='../plugins/jquery.serializeJSON/jquery.serializejson.min.js'></script>
+<script src='../plugins/fancybox/dist/jquery.fancybox.min.js'></script>
+<script src='../plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js'></script>
+<script src='../plugins/bootstrap3-dialog/dist/js/bootstrap-dialog.min.js'></script>
+<script src='../plugins/select2/dist/js/select2.full.min.js'></script>
+<script src='../js/around.js'></script>
+<script src='../js/wall.js'></script>
+<script src='../js/notifications.js'></script>
+
+<div class="footer">
+    <div class="row">
+        <div class="col-sm-6">
+        </div>
+        <div class="col-sm-6 text-left">
+            ECE Paris © 2018 All rights reserved.
+        </div>
+    </div>
+</div>
