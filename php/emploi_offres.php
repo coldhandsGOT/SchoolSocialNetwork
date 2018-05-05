a6936f1be588d2e316ac232bd44b4b2d1f40bfa8
coldhandsGOT <midearthbattle@gmail.com>
2018-05-05 16:43:27 +0200

    emploi backend up

--- php/emploi_offres.php
+++ php/emploi_offres.php
@@ -6,38 +6,66 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-        <meta http-equiv="refresh" content="0;emploi.php" />  
-
+     
 </head>
 </html>
 
 <?php
-  $query = $_GET['query']; 
-  // reçoit la valeur envoyée du search
- 
+
+ require "connexion_BDD.php";
+
+
+ $search=$_POST['search'];
+   $query = $conn->prepare("select * from emploi_offres where poste LIKE '%$search%' OR entreprise LIKE '%$search%'  LIMIT 0 , 10");
+        $query->bindValue(1, "%$search%", PDO::PARAM_STR);
+        $query->execute();
+
+        // Display search result
+              if (!$query->rowCount() == 0) {
+                echo "Search found :<br/>";
+                echo "<table style=\"font-family:arial;color:#333333;\">";  
+                        echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Title Books</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Author</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Price</td></tr>";       
+                    while ($results = $query->fetch()) {
+                echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";      
+                        echo $results['poste'];
+                echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
+                        echo $results['entreprise'];
+                echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
+                        echo "$".$results['email'];
+                echo "</td></tr>";        
+                    }
+                echo "</table>";    
+                } else {
+                    echo 'Nothing found';
+                }
+
+    
+
+
+/*
+ $result = $conn->prepare("SELECT * FROM emploi_offres WHERE (`poste` LIKE '%".$query."%') OR (`entreprise` LIKE '%".$query."%') OR (`address` LIKE '%".$query."%') OR (`email` LIKE '%".$query."%') "); 
+
+$result->execute();
+$result = $conn->prepare("SELECT FOUND_ROWS()"); 
+$result->execute();
+$row_count =$result->fetchColumn();
+echo $row_count;
+  
   
     
-    $raw_results = mysql_query("SELECT * FROM emploi_offres
-      WHERE (`poste` LIKE '%".$query."%') OR (`entreprise` LIKE '%".$query."%') OR (`address` LIKE '%".$query."%') OR (`email` LIKE '%".$query."%')") or die(mysql_error());
-      
-    // * signifie que tous les champs sont séléctionnés de la table emploi_offres
-   
-    
-    // '%$query%' ce qu'on cherche, % avant et après le query %
-    
-    if(mysql_num_rows($raw_results) > 0){ // si y'a une ou plusieurs lignes, alors:
-      
-      while($results = mysql_fetch_array($raw_results)){
-      // $results = mysql_fetch_array($raw_results) insert les données de la db dans l'array $results
-      
-        echo "<p><h3>".$results['id']."</h3>".$results['poste']."</h3>".$results['entreprise']."</h3>".$results['address']."</h3>".$results['email']."</h3>".$results['contenu']. "</p>";
-        // affiche les données, rajouté () plus tards
-      }
-      
-    }
-    else{ // if there is no matching rows do following
-      echo "No results";
-    }
+
+
+    /* display number of posts  
+$result = $conn->prepare("SELECT * FROM emploi_offres WHERE (`poste` LIKE '%".$query."%') OR (`entreprise` LIKE '%".$query."%') OR (`address` LIKE '%".$query."%') OR (`email` LIKE '%".$query."%') "); 
+
+$result->execute();
+$result = $conn->prepare("SELECT FOUND_ROWS()"); 
+$result->execute();
+$row_count =$result->fetchColumn();
+echo $row_count;
+
+*/
+
     
  
 ?>
