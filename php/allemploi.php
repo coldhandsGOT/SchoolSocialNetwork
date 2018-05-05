552d8fddb3908ec1b55252f9231c4ddaeacb3018
coldhandsGOT <midearthbattle@gmail.com>
2018-05-05 23:31:00 +0200

    commit

--- php/allemploi.php
+++ php/allemploi.php
@@ -30,10 +30,12 @@
                     	<span id="search_concept">Filter by</span> <span class="caret"></span>
                     </button>
                     <ul class="dropdown-menu" role="menu">
-                      <li><a href="#contains">Contains</a></li>
-                      <li><a href="#its_equal">It's equal</a></li>
-                      <li><a href="#greather_than">Greather than ></a></li>
-                      <li><a href="#less_than">Less than  </a></li>
+                      <li><a href="#SI">Systemes d'informations</a></li>
+                      <li><a href="#FI">Finances</a></li>
+                      <li><a href="#R">Robotique</a></li>
+                      <li><a href="#BD">Big Data</a></li>
+                      <li><a href="#SE">Systèmes Embarqués</a></li>
+
                       <li class="divider"></li>
                       <li><a href="#all">Anything</a></li>
                     </ul>
